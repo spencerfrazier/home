814fd74ee4f52291413f54bd3c881c8b0ff11d50
anaashrafi <ana.a.ashrafi@gmail.com>
2020-08-19 13:47:30 -0500

    Updates

---
+++ precache-manifest.30731c9bfcd22b1324c7581f0a87393e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6391cb61dee375b0b4495b6be7be6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ca1c7748199651eab50",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "918963bda698a5499693",
+    "url": "/static/css/main.6a3d9078.chunk.css"
+  },
+  {
+    "revision": "2ca1c7748199651eab50",
+    "url": "/static/js/2.f85e4272.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/static/js/2.f85e4272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918963bda698a5499693",
+    "url": "/static/js/main.44be4478.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "9a37dcac40f03b09c614af3d853938b1",
+    "url": "/static/media/Ana Ashrafi - Resume 2020.9a37dcac.pdf"
+  },
+  {
+    "revision": "15cb58a8ea6eda8abc68d20fc8ee97bb",
+    "url": "/static/media/Profile.15cb58a8.jpg"
+  },
+  {
+    "revision": "1096cc9af6a0b15c6fa3accce5195070",
+    "url": "/static/media/boeing.1096cc9a.jpg"
+  },
+  {
+    "revision": "241c3b8e66f1fc60ade69a3dba6e461b",
+    "url": "/static/media/committee.241c3b8e.jpg"
+  },
+  {
+    "revision": "518ff4419360e1c988e678996fa42e26",
+    "url": "/static/media/culture.518ff441.jpg"
+  },
+  {
+    "revision": "2d2b2b61779c56aa05843f899c5a9f04",
+    "url": "/static/media/custodians.2d2b2b61.jpg"
+  },
+  {
+    "revision": "fad82548bc64c3860539fda3da5aa944",
+    "url": "/static/media/dell-logo.fad82548.jpg"
+  },
+  {
+    "revision": "89c83c3b4dda7fed3a30ba8aa440bc8c",
+    "url": "/static/media/park.89c83c3b.jpg"
+  },
+  {
+    "revision": "d7e03d05cc830eda89d92c138ae9fc29",
+    "url": "/static/media/retreat.d7e03d05.jpg"
+  },
+  {
+    "revision": "0113f82a1a89a30897d85bce49f56dca",
+    "url": "/static/media/sec.0113f82a.jpg"
+  },
+  {
+    "revision": "afb11bbbfb026559897a133a1cdb8e4a",
+    "url": "/static/media/service.afb11bbb.jpg"
+  },
+  {
+    "revision": "d5cfdb857721fb2bc4c3049a34cb1ea1",
+    "url": "/static/media/zilker.d5cfdb85.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.3e14bee6a24f0c2a94e073f2605a2df3.js"
+  "/precache-manifest.30731c9bfcd22b1324c7581f0a87393e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
