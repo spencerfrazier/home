3dcea034f658322b6ff6cdf04c7ec743163dd793
Spencer Frazier <spencerjfrazier@gmail.com>
2021-04-10 17:57:11 -0400

    Fix formatting

--- src/components/home/Leadership.jsx
+++ src/components/home/Leadership.jsx
@@ -15,127 +15,21 @@ const Leadership = () => {
           <div className="container container-fluid">
             <div className="row"> 
                 <div className="col d-lg-inline align-self-center">
-                <h1 className="display-4 mb-3 text-center">Leadership</h1>
-                {/* <p className="lead text-center" style={{fontSize:'22px'}}>
-                    I empower people to acheive their goals. And I absolutely love it.
-                </p><br/><br/> */}
-                <br/>
-                <div className="row"> 
-                <div className="col d-lg-inline align-self-center">
-                  <p className="lead text-left">
-                      CTO and Founder. Primary Investigator and advisor to Chief Science Officer on quantum-encryption related research project. Technical Product Manager/ Engineering Lead for DARPA simulation effort in a team of 15+. Co-lead engineer responsible for delivering in person program updates to DARPA (as a proxy for primary investigator). Advanced Technical Leadership Class of 2019. Authored veteran warfighter financial readiness machine learning proposal for ADL/DoD in collaboration with StandardsWork. Co-authored DARPA proposal award at 7-8 figure cost estimates. Lead machine learning engineer for dismounted UAV/HMD/AR research effort. Held regular coding interviews and advised on hiring decisions. Transitioned multiple internal “Destination Innovation” research proposals to patents and IRAD programs. Delivered recruitment talks at Boston-area schools (MIT) on distributed systems and human-machine trust. Agile scrum leader/TPM on multiple projects.
-                 </p>  
-              </div>
-              
-                <div className="col d-lg-inline align-self-center">
-                {/*
-                <Carousel>
-                    <Carousel.Item>
-                {/* <div className="col d-none d-lg-inline align-self-center"> */}
-                  <img
-                    className="rounded"
-                    src={Culture}
-                    alt="culture"
-                    width="615"
-                    height="450"
-                  /> 
-                  {/* <Carousel.Caption>
-                    <h2>Student Engineering Council Vice President</h2>
-                    <h5>Cultivated an organizational culture of inclusivity, collaboration, and ambition.</h5>
-                    </Carousel.Caption> */}
-                  </Carousel.Item>
-                {/* </div>
-                </div>
-                <div className="row"> 
-                <div className="col d-none d-lg-inline align-self-center"> */}
-                <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Service}
-                    alt="service"
-                    width="615"
-                    height="450"
-                  /> 
-                  {/* <Carousel.Caption>
-                <h2>Student Engineering Council Service Director</h2>
-                <h5>Promoted student involvement in the community through volunteering and philanthropy.</h5>
-                </Carousel.Caption> */}
-                  </Carousel.Item>
-                {/* </div>
-                </div> */}
-                {/* <div className="row"> 
-                <div className="col d-none d-lg-inline align-self-center"> */}
-                {/* <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Code}
-                    alt="codeorange"
-                    width="300"
-                    height="475"
-                  /> 
-                  </Carousel.Item> */}
-                {/* </div>
-                </div> */}
-                <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Sec}
-                    alt="SEC"
-                    width="615"
-                    height="450"
-                  /> 
-                  </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Zilker}
-                    alt="zilker"
-                    width="615"
-                    height="450"
-                  /> 
-                  </Carousel.Item>
-                  <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Custodians}
-                    alt="custodialappreciation"
-                    width="615"
-                    height="450"
-                  /> 
-                  </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Committee}
-                    alt="committee"
-                    width="615"
-                    height="450"
-                  /> 
-                  </Carousel.Item>
-                  <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Park}
-                    alt="volunteer"
-                    width="615"
-                    height="450"
-                  /> 
-                  </Carousel.Item>
-                  <Carousel.Item>
-                  <img
-                    className="rounded"
-                    src={Retreat}
-                    alt="family"
-                    width="615"
-                    height="450"
-                  /> 
-                  </Carousel.Item>
-                </Carousel>*/}
-              </div>
+                  <h1 className="display-4 mb-3 text-center">Leadership</h1>
+                  {/* <p className="lead text-center" style={{fontSize:'22px'}}>
+                      I empower people to acheive their goals. And I absolutely love it.
+                  </p><br/><br/> */}
+                  <br/>
+                  <div className="row"> 
+                    <div className="col d-lg-inline align-self-center">
+                      <p className="lead text-left">
+                          CTO and Founder. Primary Investigator and advisor to Chief Science Officer on quantum-encryption related research project. Technical Product Manager/ Engineering Lead for DARPA simulation effort in a team of 15+. Co-lead engineer responsible for delivering in person program updates to DARPA (as a proxy for primary investigator). Advanced Technical Leadership Class of 2019. Authored veteran warfighter financial readiness machine learning proposal for ADL/DoD in collaboration with StandardsWork. Co-authored DARPA proposal award at 7-8 figure cost estimates. Lead machine learning engineer for dismounted UAV/HMD/AR research effort. Held regular coding interviews and advised on hiring decisions. Transitioned multiple internal “Destination Innovation” research proposals to patents and IRAD programs. Delivered recruitment talks at Boston-area schools (MIT) on distributed systems and human-machine trust. Agile scrum leader/TPM on multiple projects.
+                     </p>  
+                    </div>
+                  </div>
               </div>
             </div>
           </div>
-      </div>
       </div>
       );
     };
